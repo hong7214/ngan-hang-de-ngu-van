7f01fafb54dfbb1ede8646275a46b599acb447b6
hong7214 <taikhoanchungtestgame@gmail.com>
2026-09-22 19:23:36 +0700

    Create next.config.mjs

---
+++ ngan-hang-de-ngu-van-starter/next.config.mjs
@@ -0,0 +1,13 @@
+const nextConfig = {
+  output: "export",
+
+  basePath: "/ngan-hang-de-ngu-van",
+
+  images: {
+    unoptimized: true,
+  },
+
+  trailingSlash: true,
+};
+
+export default nextConfig;
